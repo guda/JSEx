bf18f4fe75ec787406133aee1042730ac4c9ca17
TriLLi <trillix@gmail.com>
2016-11-12 11:44:52 +0100

    firstr commit

---
+++ js/JSExString.js
@@ -0,0 +1,39 @@
+/*
+    JavaScript String Extensions v1.0.0
+    Author Senad Meškin, 2016.
+*/
+if (!String.prototype.contains) {
+    String.prototype.contains = function (s) {
+        return this.toString().indexOf(s) > -1;
+    }
+}
+/*
+    Convert string to array passing separator, or to array of arrays passing second separator
+*/
+if (!String.prototype.toArray) {
+    String.prototype.toArray = function (s1, s2) {
+        var s = this.toString();
+        if (s2 == null) {
+            return s.toString().split(s1);
+        } else {
+            var d = s.toArray(s1);
+            var r = [];
+            for (var x in d) {
+                r.push(d[x].split(s2));
+            }
+            return r;
+        }
+    }
+}
+//Check if string is email
+if (!String.prototype.isEmail) {
+    String.prototype.isEmail = function () {
+        return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.toString());
+    }
+}
+//trim string
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  };
+}
